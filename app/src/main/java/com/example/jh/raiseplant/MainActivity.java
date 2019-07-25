c68480eec5ba452cf12fca3686a486e29bad6ecc
Jihye Lee <dusrhs37@gmail.com>
2019-07-25 12:42:33 +0900

    base ui with icon

--- app/src/main/java/com/example/jh/raiseplant/MainActivity.java
+++ app/src/main/java/com/example/jh/raiseplant/MainActivity.java
@@ -2,25 +2,37 @@ package com.example.jh.raiseplant;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+
+    // 4개의 메뉴에 들어갈 Fragment들
+    private MyPlantFragment myPlantFragment = new MyPlantFragment();
+    private SelfCheckFragment selfCheckFragment = new SelfCheckFragment();
+    private DrCheckFragment dr_check_fragment = new DrCheckFragment();
+    private MypageFragment mypage_fagment = new MypageFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-
         CustomBottomNavigationView curvedBottomNavigationView = findViewById(R.id.customBottomBar);
-        curvedBottomNavigationView.inflateMenu(R.menu.navigation);
         curvedBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        curvedBottomNavigationView.setItemIconTintList(null);
+
+        // 첫 화면 지정
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, myPlantFragment
+        ).commitAllowingStateLoss();
     }
 
     private CustomBottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -28,22 +40,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.one:
-                    mTextMessage.setText(R.string.title_home);
                     //화면전환하기
-                    return true;
+                    transaction.replace(R.id.frame_layout, myPlantFragment).commitAllowingStateLoss();
+                    transaction.addToBackStack(null);
+                    break;
                 case R.id.two:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    transaction.replace(R.id.frame_layout, selfCheckFragment).commitAllowingStateLoss();
+                    transaction.addToBackStack(null);
+                    break;
                 case R.id.four:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    transaction.replace(R.id.frame_layout, dr_check_fragment).commitAllowingStateLoss();
+                    transaction.addToBackStack(null);
+                    break;
                 case R.id.five:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    transaction.replace(R.id.frame_layout, mypage_fagment).commitAllowingStateLoss();
+                    transaction.addToBackStack(null);
+                    break;
             }
-            return false;
+            return true;
         }
     };
 
